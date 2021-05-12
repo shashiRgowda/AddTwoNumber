3767d8a9a7b1d95ea371942b32a166600f6607b1
shashiRgowda <shashikumartr64@gmail.com>
2021-05-12 13:56:41 +0530

    Added:AddTwoNumbers

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(String [] args)
+        { 
+        
+        }
+    
+        public int Add(int numOne, int numTwo);
+        {
+            int sum = numOne + numTwo;
+            return sum;
+                             
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program program = new Program();
+            int result = program.Add(5, 10);
+            Assert.AreEqual(15, result);
+        }
+    }
+}
